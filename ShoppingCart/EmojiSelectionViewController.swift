3902401bb39491fe3cddf43378bb562c8930a623
Michael Florez <mike_florez@ymail.com>
2017-02-27 11:27:59 -0500

    done

--- ShoppingCart/EmojiSelectionViewController.swift
+++ ShoppingCart/EmojiSelectionViewController.swift
@@ -10,6 +10,24 @@ import UIKit
 
 class EmojiSelectionViewController: UIViewController {
     
+    @IBOutlet weak var firstEmojiTextField: UITextField!
+    @IBOutlet weak var secondEmojiTextField: UITextField!
+    @IBAction func emoijButtonTapped(_ sender: Any) {
+        
+        
+        
+        
+        guard let firstText = firstEmojiTextField.text,
+            let secondText = secondEmojiTextField.text else {return}
+        let tupleString = (firstText, secondText)
+        emojiDelegate?.create(emojiGroup: tupleString)
+        
+        
+        dismiss(animated: true, completion: nil)
+    }
+    
+    var emojiDelegate: EmojiCreation?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = UIColor(red:0.22, green:0.33, blue:0.58, alpha:1.00)
--- ShoppingCart/ShoppingListViewController.swift
+++ ShoppingCart/ShoppingListViewController.swift
@@ -9,14 +9,20 @@
 import UIKit
 
 
-// TODO: Create protocol here.
+protocol EmojiCreation {
+    func create(emojiGroup: (String, String))
+}
 
 
 class ShoppingViewController: UIViewController {
     
     @IBOutlet weak var tableView: UITableView!
     
-    var emojis: [(String, String)] = []
+    var emojis: [(String, String)] = [] {
+        didSet {
+            tableView.reloadData()
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,6 +31,12 @@ class ShoppingViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
         tableView.allowsSelection = false
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destVC = segue.destination as! EmojiSelectionViewController
+        destVC.emojiDelegate = self
+        
     }
     
 }
@@ -47,6 +59,13 @@ extension ShoppingViewController: UITableViewDataSource {
 
 // MARK: - UITableViewDelegate Methods
 extension ShoppingViewController: UITableViewDelegate { }
+
+extension ShoppingViewController: EmojiCreation {
+    
+    func create(emojiGroup: (String, String)) {
+        emojis.append(emojiGroup)
+    }
+}
 
 
 
